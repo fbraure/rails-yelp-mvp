0e40bc9c6f035620272003f03b265431a7df53a5
Florent BRAURE <florent.braure@gmail.com>
2020-11-11 17:08:22 +0100

    Save with render review_new in restaurant

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -6,6 +6,7 @@ class RestaurantsController < ApplicationController
   end
 
   def show
+    @new_review = Review.new
     @reviews = Review.where(:restaurant_id == @restaurant.id)
   end
 
--- app/models/review.rb
+++ app/models/review.rb
@@ -3,6 +3,7 @@ class Review < ApplicationRecord
 
   validates :rating, numericality: { only_integer: true, greater_than_or_equal_to: 0, less_than_or_equal_to: 5 }
   # presence: true, inclusion: { in: (1..5).to_a }
+  # inclusion: { in: [0,1,2,3], allow_nil: false }
   validates :content, presence: true, exclusion: { in: [""] }
 
 end
